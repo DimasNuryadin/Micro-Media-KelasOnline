9d1a1bf632f20e20b73f7bc176bda8fb0af6378d
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2025-01-10 20:02:02 +0700

    Setup project and setup sequelize

---
+++ routes/media.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img')
+
+/* GET users listing. */
+router.post('/', (req, res) => {
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: 'error', message: 'invalid base64' });
+  }
+
+  return res.send("ok")
+});
+
+module.exports = router;
